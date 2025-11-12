fa387638f9eb261099a1770dfaab8fdcef558c1d
Termiter181 <miguelpokemonalves@gmail.com>
2025-11-12 18:23:44 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Cloud VPN App</h1>
+      <p>Em breve seu código VPN aqui!</p>
+    </div>
+  )
+}
+
+export default App
